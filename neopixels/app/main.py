6a1d472c79f321784add865e68f9aa14e1d37824
MarlTake <otake.shigenori@gmail.com>
2020-11-08 14:01:51 +0000

    run in sub process

--- neopixels/app/main.py
+++ neopixels/app/main.py
@@ -1,10 +1,18 @@
+import multiprocessing
+import os
+import signal
+import time
 from . import pattern, neo_pixel
 
 
 def main():
-    generator = pattern.generators[1]
+    pattern_index = 2
+    generator = pattern.generators[pattern_index]
     print(generator)
-    neo_pixel.run(generator)
+    proc = multiprocessing.Process(target=neo_pixel.run, args=(generator,))
+    proc.start()
+    time.sleep(4)
+    os.kill(proc.pid, signal.SIGINT)
 
 
 if __name__ == "__main__":
